c3fc6212ffd879e29a8992e9e4c0b6ada35825a5
aralche <aralcheghizul@gmail.com>
2023-06-07 22:32:30 +0700

    Menambah List & paragraph

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hellow MDFK!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui natus at inventore ipsum sit, nemo deserunt dicta a expedita tempora. Dolorem voluptatibus officiis mollitia placeat exercitationem, illo velit necessitatibus neque.</p>
+    <ul>
+        <li>ONE</li>
+        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quisquam dolor ad ut eum adipisci officiis, et quia eos culpa, nulla similique maxime! Perferendis dolorum delectus laudantium ipsam similique sequi?</P>
+    </ul>
 </body>
 </html>
